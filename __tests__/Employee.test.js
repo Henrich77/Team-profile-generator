d7926b12165850d0343af12b1d38a69ed12c20ff
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-01-25 16:06:19 -0500

    added comments and starter for Employee test js

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,35 @@
+// require Employee
+
+
+// write a test to see if you can instantiate an Employee instance
+test( 'can instantiate Employee instance', () =>{
+
+    // variable for the new Employee
+    // expect the type of that variable to be an object
+});
+
+// write a test to see if you can set the name of the constructor argument
+
+test ( 'can set the name via constructor arguments', () => {
+
+    // variable for the name 
+    //variable for a new employee with the name passed in as an argument
+    // expect the variable.name to be the variable that you can set up for the name
+})
+
+// same as above for id 
+
+
+// same as above for email
+
+// write a test to see if your getter method is working
+test (' can get name via getName', () => {
+    //variable for the test value
+    //variable for the new employee with the test value passed in 
+    // expect that when you call that method - to be the test value variable
+}) 
+
+
+// same for getting the id
+// same for getting the email 
+// same for getting the role
