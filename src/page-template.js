048a633ef45721f0de803b51a694267ad1b4fc23
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-01-26 23:01:30 -0500

    fixed an issue with the array.map() in the genearateTeam()

--- src/page-template.js
+++ src/page-template.js
@@ -1,100 +1,90 @@
 // create the team
-
+// const Engineer = require('./lib/Engineer');
 const Engineer = require("../lib/Engineer");
+// const Employee = require("./lib/Employee");
 
 
 const generateTeam = team => {
 
-    console.log(team)
-    team.map((teamMember, index) => {
+    const teamHTML = team.map((teamMember, index) => {
         console.log(teamMember.getRole())
+        console.log(teamMember.getName())
+
 
         if (teamMember.getRole() === 'Engineer') {
 
             return `
+
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                     <div class="card-body">
+                        <h5 class="card-title">${teamMember.getName()}</h5>
+                        <p class="card-text">${teamMember.getGithub()}</p>
+                        <a href="#" class="btn btn-primary">${teamMember.getEmail()}</a>
+                    </div>
+                </div>
         
-            <div>
-                <h1> ${teamMember.getName()} </h1>
-            <div>    
+             
             `
         }
         else if (teamMember.getRole() === 'Manager') {
 
             return `
-        
-            <div>
-                <h1> ${teamMember.getName()} </h1>
-            <div>    
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">C</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+           
             `
         }
         else if (teamMember.getRole() === 'Intern') {
 
             return `
         
-            <div>
-                <h1> ${teamMember.getName()} </h1>
-            <div>    
+            <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+             </div>
             `
         }
 
 
-    })
+    }).join('')
 
-    // create manager html 
-    // const generateManager = manager => {
-    //     return `
-        
-    //     <div>
-    //         <h1> ${manager.getName()} </h1>
-    //     <div>    
-    //     `
-    // };
-    // create the html for the engineer
-    // const generateEngineer = engineer => {
-    //     return `
-        
-    //     <div>
-    //         <h1> ${engineer.getName()} </h1>
-    //     <div>    
-    //     `
-
-    // };
-    // create the html for the intern
-    // const generateIntern = intern => {
-    //     return `
-        
-    //     <div>
-    //         <h1> ${intern.getName()} </h1>
-    //     <div>    
-    //     `
-
-    // };
-
-    // const hmtl = [];
-
-    //push the team array that is passed to this function into the empty html array
-    // get just the employees out of the html array that have a role of Manager 
-    //call the generateManager function, passing in that manager 
-
-    // same thing , but for employees that have engineer role 
-    // at the end , join the array together
-
-    // same thing, but for employees that have intern role 
-    // at the end , join the array together 
-
-    // return the joined array
-
-
-
-
-
+    return teamHTML
 }
+// const hmtl = [];
+
+//push the team array that is passed to this function into the empty html array
+// get just the employees out of the html array that have a role of Manager 
+//call the generateManager function, passing in that manager 
+
+// same thing , but for employees that have engineer role 
+// at the end , join the array together
+
+// same thing, but for employees that have intern role 
+// at the end , join the array together 
+
+// return the joined array
+
+
+
+
+
 
 
 // export function to generate the entire page 
 
 module.exports = team => {
-    console.log(team)
+    console.log('this is the html with the team generated', team)
 
     return `
     <!DOCTYPE html>
@@ -103,10 +93,25 @@ module.exports = team => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    <div> ${generateTeam(team)}</div>
+    <header>
+    <h1>The guys</h1>
+    </header>
+
+    <main>${generateTeam(team)}</main>
+    
+    
+    
+    
+    
+    
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html> 
     `
